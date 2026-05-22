f45317dcb4102986055ce32733aed25d991e81e1
Samba SIDIBE <sambasidibe@gmail.com>
2026-05-22 09:36:51 +0000

    Initialisation du projet, ajout des librairies de base spring boot

---
+++ src/main/java/sn/edu/isepat/dbe/p6/gestion_pharma/GestionPharmaApplication.java
@@ -0,0 +1,13 @@
+package sn.edu.isepat.dbe.p6.gestion_pharma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPharmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPharmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/edu/isepat/dbe/p6/gestion_pharma/GestionPharmaApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.edu.isepat.dbe.p6.gestion_pharma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPharmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
